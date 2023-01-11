80abb024703919ed41a07f0bab5fa1fd2848f41e
Isabella Mensatto Baccaro <you@example.com>
2023-01-11 08:41:00 -0300

    refatorado accordionlist

--- js/script.js
+++ js/script.js
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 import ScrollSuave from './modules/scroll-suave.js';
-=======
->>>>>>> 548996ff31b745ad8b1f44f1ba09f055e4826514
-import initAnimacaoScroll from './modules/scroll-animacao.js';
-import initAccordion from './modules/accordion.js';
+import Accordion from './modules/accordion.js';
 import initTabNav from './modules/tabnav.js';
 import initModal from './modules/modal.js';
 import initTooltip from './modules/tooltip.js';
@@ -12,19 +8,14 @@ import initMenuMobile from './modules/menu-mobile.js';
 import initFuncionamento from './modules/funcionamento.js';
 import initFetchAnimais from './modules/fetch-animais.js';
 import initFetchBitcoin from './modules/fetch-bitcoin.js';
-import ScrollSuave from './modules/scroll-suave.js';
+import initAnimacaoScroll from './modules/scroll-animacao.js';
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
 
-<<<<<<< HEAD
-const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
-scrollSuave.init();
+const accordion = new Accordion('[data-anime="accordion"] dt');
+accordion.init();
 
-=======
->>>>>>> 548996ff31b745ad8b1f44f1ba09f055e4826514
-initAnimacaoScroll();
-initAccordion();
 initTabNav();
 initModal();
 initTooltip();
@@ -33,3 +24,4 @@ initMenuMobile();
 initFuncionamento();
 initFetchAnimais();
 initFetchBitcoin();
+initAnimacaoScroll();
