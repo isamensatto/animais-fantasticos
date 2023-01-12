a6c54c1fb665ed337ed6169095194fd072aeb912
Isabella Mensatto Baccaro <you@example.com>
2023-01-12 08:38:48 -0300

    refac

--- js/modules/tabnav.js
+++ js/modules/tabnav.js
@@ -1,31 +1,61 @@
-export default class TabNav {
-  constructor(menu, content) {
-    this.tabMenu = document.querySelectorAll(menu);
-    this.tabContent = document.querySelectorAll(content);
-    this.activeClass = 'ativo';
+export default class Tooltip {
+  constructor(tooltips) {
+    this.tooltips = document.querySelectorAll(tooltips);
+
+    // bind do objeto da classe aos callbacks
+    this.onMouseLeave = this.onMouseLeave.bind(this);
+    this.onMouseMove = this.onMouseMove.bind(this);
+    this.onMouseOver = this.onMouseOver.bind(this);
   }
 
-  // Ativa a tab de acordo com o index da mesma
-  activeTab(index) {
-    this.tabContent.forEach((section) => {
-      section.classList.remove(this.activeClass);
-    });
-    const direcao = this.tabContent[index].dataset.anime;
-    this.tabContent[index].classList.add(this.activeClass, direcao);
+  // Move a tooltip com base em seus estilos
+  // de acordo com a posição do mouse
+  onMouseMove(event) {
+    this.tooltipBox.style.top = `${event.pageY + 20}px`;
+    if (event.pageX + 240 > window.innerWidth) {
+      this.tooltipBox.style.left = `${event.pageX - 190}px`;
+    } else {
+      this.tooltipBox.style.left = `${event.pageX + 20}px`;
+    }
   }
 
-  // Adiciona os eventos nas tabs
-  addTabNavEvent() {
-    this.tabMenu.forEach((itemMenu, index) => {
-      itemMenu.addEventListener('click', () => this.activeTab(index));
+  // Remove a tooltip e os eventos de mousemove e mouseleave
+  onMouseLeave({ currentTarget }) {
+    this.tooltipBox.remove();
+    currentTarget.removeEventListener('mouseleave', this.onMouseLeave);
+    currentTarget.removeEventListener('mousemove', this.onMouseMove);
+  }
+
+  // Cria a tooltip box e coloca no body
+  criarTooltipBox(element) {
+    const tooltipBox = document.createElement('div');
+    const text = element.getAttribute('aria-label');
+    tooltipBox.classList.add('tooltip');
+    tooltipBox.innerText = text;
+    document.body.appendChild(tooltipBox);
+    this.tooltipBox = tooltipBox;
+  }
+
+  // Cria a tooltip e adiciona os eventos
+  // de mousemove e mouseleave ao target
+  onMouseOver({ currentTarget }) {
+    // cria a tooltipbox e coloca em uma propriedade
+    this.criarTooltipBox(currentTarget);
+    currentTarget.addEventListener('mousemove', this.onMouseMove);
+    currentTarget.addEventListener('mouseleave', this.onMouseLeave);
+  }
+
+  // Adiciona os eventos de mouseover a cada tooltip
+  addTooltipsEvent() {
+    this.tooltips.forEach((item) => {
+      item.addEventListener('mouseover', this.onMouseOver);
     });
   }
 
   init() {
-    if (this.tabMenu.length && this.tabContent.length) {
-      // ativar primeiro item
-      this.activeTab(0);
-      this.addTabNavEvent();
+    if (this.tooltips.length) {
+      this.addTooltipsEvent();
     }
+    return this;
   }
 }
